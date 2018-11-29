323de6b0ff6e154c9d8e2a071a36e1ae5aec2c65
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-11-29 10:28:46 -0200

    EndPoint /clientes/{id} disponivel

--- src/main/java/com/hzm/cursomc/resources/ClienteResource.java
+++ src/main/java/com/hzm/cursomc/resources/ClienteResource.java
@@ -1,6 +1,5 @@
 package com.hzm.cursomc.resources;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hzm.cursomc.domain.Categoria;
-import com.hzm.cursomc.services.CategoriaService;
+import com.hzm.cursomc.domain.Cliente;
+import com.hzm.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CateroriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
