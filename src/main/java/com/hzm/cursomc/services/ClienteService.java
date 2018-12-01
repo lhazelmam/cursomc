93f74ecae5a56579249f2c5585f415d1064db9b0
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-12-01 19:05:30 -0200

    PUT, DELETE e GET para clientes

--- src/main/java/com/hzm/cursomc/services/ClienteService.java
+++ src/main/java/com/hzm/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.hzm.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.hzm.cursomc.domain.Cliente;
+import com.hzm.cursomc.dto.ClienteDTO;
 import com.hzm.cursomc.repositories.ClienteRepository;
+import com.hzm.cursomc.services.exceptions.DataIntegrityException;
 import com.hzm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -24,5 +31,39 @@ public class ClienteService {
 		
 	}
 	
+	public Cliente update(Cliente obj) {
+		Cliente newObj =   find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		}
+		catch (DataIntegrityViolationException e ) {
+			throw new DataIntegrityException("nao é possível excluir um cliente pois há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}
+	
+	private void updateData (Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
 }
