b6daca43883725d06dca312043acaa8cecb0d55e
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-12-01 07:40:24 -0200

    Listando todas as Categorias com DTO

--- src/main/java/com/hzm/cursomc/resources/CateroriaResource.java
+++ src/main/java/com/hzm/cursomc/resources/CateroriaResource.java
@@ -2,6 +2,8 @@ package com.hzm.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hzm.cursomc.domain.Categoria;
+import com.hzm.cursomc.dto.CategoriaDTO;
 import com.hzm.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,5 +54,11 @@ public class CateroriaResource {
 		return ResponseEntity.noContent().build();
 	}
 		
-	
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);	
+	}
 }
