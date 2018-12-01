0a6704d6295adf73181516d934e738fc015db0c7
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-12-01 10:30:05 -0200

    Validacao sintatica com Bean Validation

--- src/main/java/com/hzm/cursomc/services/CategoriaService.java
+++ src/main/java/com/hzm/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.hzm.cursomc.domain.Categoria;
+import com.hzm.cursomc.dto.CategoriaDTO;
 import com.hzm.cursomc.repositories.CategoriaRepository;
 import com.hzm.cursomc.services.exceptions.DataIntegrityException;
 import com.hzm.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+		
+	}
 	
 }
 
