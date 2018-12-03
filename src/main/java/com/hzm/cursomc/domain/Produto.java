8dbbbc447d90012b6aedecb038f6660665989dec
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-12-03 08:18:29 -0200

    Busca de produtos por nome e categorias

--- src/main/java/com/hzm/cursomc/domain/Produto.java
+++ src/main/java/com/hzm/cursomc/domain/Produto.java
@@ -19,30 +19,25 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA",
-		joinColumns = @JoinColumn(name = "produto_id"),
-		inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
-	
+
 	@JsonIgnore
-	@OneToMany(mappedBy="id.produto")
+	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
-	
-	
+
 	public Produto() {
-		
+
 	}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -51,7 +46,7 @@ public class Produto implements Serializable {
 		this.nome = nome;
 		this.preco = preco;
 	}
-	
+
 	@JsonIgnore
 	public List<Pedido> pedidos() {
 		List<Pedido> lista = new ArrayList<>();
@@ -59,9 +54,8 @@ public class Produto implements Serializable {
 			lista.add(x.getPedido());
 		}
 		return lista;
-		
+
 	}
-	
 
 	public Integer getId() {
 		return id;
@@ -94,7 +88,7 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
-	
+
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
@@ -102,8 +96,6 @@ public class Produto implements Serializable {
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	};
-	
-	
 
 	@Override
 	public int hashCode() {
@@ -129,7 +121,5 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-
-
 
 }
--- src/main/java/com/hzm/cursomc/services/CategoriaService.java
+++ src/main/java/com/hzm/cursomc/services/CategoriaService.java
@@ -18,23 +18,23 @@ import com.hzm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		Categoria newObj =   find(obj.getId());
+		Categoria newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
@@ -42,32 +42,29 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repo.deleteById(id);			
-		}
-		catch (DataIntegrityViolationException e ) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("nao eh possivel excluir uma categoria que possui produtos");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
-		
+
 	}
-	
-	private void updateData (Categoria newObj, Categoria obj) {
+
+	private void updateData(Categoria newObj, Categoria obj) {
 		newObj.setNome(obj.getNome());
-		
+
 	}
-	
+
 }
-
-
