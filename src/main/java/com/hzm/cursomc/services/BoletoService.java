fb3ee2a5dc31d5e3eaaf8b0eeb67309addf89fa5
leandro Hazelmam <leandro.hazelmam@gmail.com>
2018-12-10 17:32:15 -0200

    Criando Profile de Teste

--- src/main/java/com/hzm/cursomc/services/BoletoService.java
+++ src/main/java/com/hzm/cursomc/services/BoletoService.java
@@ -3,18 +3,21 @@ package com.hzm.cursomc.services;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.springframework.stereotype.Service;
+
 import com.hzm.cursomc.domain.PagamentoComBoleto;
 
+@Service
 public class BoletoService {
 
-	//em e-commerce, esse metodo seria substituido pela utilizacao de um WS de geracao de boleto, que traria a data de vencimento do boleto 
-	
-	
+	// em e-commerce, esse metodo seria substituido pela utilizacao de um WS de
+	// geracao de boleto, que traria a data de vencimento do boleto
+
 	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(instanteDoPedido);
-		cal.add(Calendar.DAY_OF_MONTH,7);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
 		pagto.setDataVencimento(cal.getTime());
-	
+
 	}
 }
